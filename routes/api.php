3d0cd92df24e6b8b874a929763329ebfa2e706db
Muhammad Farid Gilang Nuswantoro <faridganteng20050@gmail.com>
2023-06-21 20:56:45 +0700

    api : create, update, delete data

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\API\CategoryController;
 use App\Http\Controllers\API\ProfileController;
 use App\Http\Controllers\API\TodoController;
 use Symfony\Component\HttpKernel\Profiler\Profile;
@@ -19,10 +20,16 @@ use Symfony\Component\HttpKernel\Profiler\Profile;
 */
 
 Route::post('/login', [AuthController::class, 'login'])->name('login');
-Route::middleware('auth:sanctum')->group(function(){
+Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
     Route::get('/profile', [ProfileController::class, 'index'])->name('profile');
 
+    //Route::resource('/todo', TodoController::class);
     Route::apiResource('/todo', TodoController::class);
-    
+
+    //Route:apiResource('/category', CategoryController::class);
+    Route::get('/category', [CategoryController::class, 'index'])->name('category.index');
+    Route::patch('/todo/{todo}/complete', [TodoController::class, 'complete'])->name('todo.complete');
+    Route::patch('/todo/{todo}/uncomplete', [TodoController::class, 'uncomplete'])->name('todo.uncomplete');
+    Route::delete('/todo', [TodoController::class, 'deleteAllCompleted'])->name('todo.deleteallcompleted');
 });
